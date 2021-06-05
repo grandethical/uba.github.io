efa5bde2647c2056f88a7c53d7cc27675b250dc4
Anand kumar yadav <anandsamrat44@gmail.com>
2021-06-06 03:02:13 +0530

    United Bank of Asgard

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+
+    <title>United bank of Asgard</title>
+</head>
+<body>
+    <nav class="navbar background">
+        <ul class="nav-list">
+            
+            <div class="logo"><img src="logo.jpeg" alt="logo"></div>
+            
+            <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+           <p class="logo-text"><strong>United Bank of Asgard</strong></p>
+</nav>
+          
+        </ul>
+        
+            
+            
+         </form>
+
+        </div>
+    </nav>
+    <section class="background firstsection">
+        <div class="box-main">
+            <div class="firsthalf">
+                <p class="text-big"><strong>United Bank of Asgard</strong></p>
+                <p><em>"Transfer your money easily and securely."</em></p>
+                <div class="button">
+                  <button class="btn"><b><a href="viewusers.php">Customer Details</a></b>
+                  
+                  </button>
+                  <button class="btn"><b><a href="transactionDetails.php">Transaction History</a></b>
+                  
+                    </button>
+                    
+                   
+                  
+                </div>
+            </div>
+
+            <div class="secondhalf">
+                <img src="logo.jpeg" alt="extra">
+                
+                
+            </div>
+</section>
+
+ <!-- Activity section -->
+ <div class="row activity text-center">
+                 
+                  </div>
+            </div>
+      </div>
+      <footer class="text-center mt-5 py-2">
+      <style>
+a:link {
+  color: green;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:visited {
+  color: black;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:hover {
+  color: red;
+  background-color: transparent;
+  text-decoration: underline;
+}
+a:active {
+  color: yellow;
+  background-color: transparent;
+  text-decoration: underline;
+}
+</style>
+
+
+
+  <footer class="background">
+      <p class="text-footer">
+
+        Copyright &copy; 2021- Anand Kumar Yadav - All rights reserved
+        <br>The Sparks Foundation</br>
+      </p>
+  </footer>  
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  
+</body>
+
+</html>
